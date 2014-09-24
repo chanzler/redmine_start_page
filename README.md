c3abb8a85d0261fb03b1c01014119d12c0dcf085
Frank Kanzler <frank@intersolve.de>
2014-09-24 23:22:14 +0200

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# redmine start page - Redmine plugin
+
+This plugin redirects the user, just after he has logged, to the URL defined in settings. When auto-logged-out the user will be redirected to the page he last visited. If the redirect url is not defined (default), the behaviour is as usual.
+
+## Installation
+
+    git clone https://github.com/chanzler/redmine_start_page
+
+Restart Redmine's server.
+
+## Configuration
+
+Go to `Administration` > `Plugins` > `Configure` to set the URL for the new start page.
+
+Enter the relative URL beginning with a slash (/my/page) or the absolute, FQDN URL (http://www.example.com/landing-page)
---
+++ init.rb
@@ -0,0 +1,27 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_start_page do
+  name 'Set a new start page after login in redmine'
+  author 'Frank Kanzler (intersolve)'
+  description 'This plugin redirects the user, just after he has logged in, to the URL defined in settings. When auto-logged-out the user will be redirected to the page he last visited.'
+  version '0.1.0'
+  url 'https://github.com/chanzler/redmine_start_page'
+  author_url 'https://github.com/chanzler/'
+
+  settings(:default => { 'redirect' => '' }, :partial => 'settings/redirect_to_after_login_settings' )
+end
+
+module Hooks
+  class Hooks < Redmine::Hook::ViewListener
+     def controller_account_success_authentication_after(context={})
+         startPage_url = Setting[:plugin_redmine_start_page][:startPage]
+         Rails.logger.info context[:request].params[:back_url]
+         Rails.logger.info "Hostneme: " 
+         Rails.logger.info "#{Setting.protocol}://#{Setting.host_name}"
+         if context[:request].params[:back_url] == "#{Setting.protocol}://#{Setting.host_name}/"
+           context[:request].params[:back_url] = startPage_url
+         end
+         return ''
+     end
+  end
+end
